5b5e39c4655222cb6999629010fbf11e23d515fc
Jakob <45147090+jakob6124@users.noreply.github.com>
2019-10-20 21:01:02 +0200

    Changed variable names

--- main.h
+++ main.h
@@ -13,13 +13,13 @@ namespace virtual
 {
 template <class val>
 std::vector<DWORD> ScanProcessMemory(HANDLE ProcessHandle, std::vector<val> Value ,bool space, DWORD minimum, DWORD maximum) {
-std::vector<DWORD>Locations; bool break = false; INT64 mem; if (!space) { mem = 0; } else { mem = minimum; }
+std::vector<DWORD>Locations; bool breakop = false; INT64 mem; if (!space) { mem = 0; } else { mem = minimum; }
 if (!space) {
 MEMORY_BASIC_INFORMATION Memory;
-while (VirtualQueryEx(ProcessHandle, (LPVOID)mem, &Memory, sizeof(MEMORY_BASIC_INFORMATION))) {
-if (Memory.State == MEM_COMMIT) {
+while (VirtualQueryEx(ProcessHandle, (LPVOID)mem, &mem, sizeof(MEMORY_BASIC_INFORMATION))) {
+if (mem.State == MEM_COMMIT) {
 std::vector<val> Buffer(Memory.RegionSize);
-if (ReadProcessMemory(ProcessHandle, (LPVOID)mem, &Buffer[0], Memory.RegionSize, 0)) {
+if (ReadProcessMemory(ProcessHandle, (LPVOID)mem, &Buffer[0], mem.RegionSize, 0)) {
 for (size_t i = 0; i < Buffer.size(); ++i) {
 for (auto f : Value) {
 if (Buffer[i] == f) {
@@ -28,7 +28,7 @@ Locations.push_back(mem + ((i + 1) * sizeof(val)) - sizeof(val));
 }
 }
 }
-} mem += Memory.RegionSize;
+} mem += mem.RegionSize;
 }
 }
 else {
@@ -37,16 +37,16 @@ std::vector<val> Buffer(MEMBLOCK);
 if (ReadProcessMemory(ProcessHandle, (LPVOID)mem, &Buffer[0], MEMBLOCK, 0)) {
 for (size_t i = 0; i < Buffer.size(); ++i) {
 if (mem + ((i + 1) * sizeof(val)) - sizeof(val) > maximum) {
-break = true;
+breakop = true;
 break;
 }
 for (auto f : Value) {
 if (Buffer[i] == f) {
 Locations.push_back(mem + ((i + 1) * sizeof(val)) - sizeof(val));
 }
-} if (break) { break; }
+} if (breakop) { break; }
 }
-} if (break) { break; } mem += MEMBLOCK;
+} if (breakop) { break; } mem += MEMBLOCK;
 }
 }return Locations;
 }
