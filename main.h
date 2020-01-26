5ac1c2fad1625be03e331e1ef21471f494977022
Jakob <45147090+jakob6124@users.noreply.github.com>
2020-01-26 16:46:08 +0100

    Now the spacing is fine again

--- main.h
+++ main.h
@@ -9,46 +9,47 @@
 
 namespace virtualmemory
 {
-namespace virtual
-{
-template <class val>
-std::vector<DWORD> ScanProcessMemory(HANDLE ProcessHandle, std::vector<val> Value ,bool space, DWORD minimum, DWORD maximum) {
-std::vector<DWORD>Locations; bool breakop = false; INT64 mem; if (!space) { mem = 0; } else { mem = minimum; }
-if (!space) {
-MEMORY_BASIC_INFORMATION Memory;
-while (VirtualQueryEx(ProcessHandle, (LPVOID)mem, &mem, sizeof(MEMORY_BASIC_INFORMATION))) {
-if (mem.State == MEM_COMMIT) {
-std::vector<val> Buffer(Memory.RegionSize);
-if (ReadProcessMemory(ProcessHandle, (LPVOID)mem, &Buffer[0], mem.RegionSize, 0)) {
-for (size_t i = 0; i < Buffer.size(); ++i) {
-for (auto f : Value) {
-if (Buffer[i] == f) {
-Locations.push_back(mem + ((i + 1) * sizeof(val)) - sizeof(val));
-      }
+    namespace virtual
+    {
+        template <class val>
+        std::vector<DWORD> ScanProcessMemory(HANDLE ProcessHandle, std::vector<val> Value, bool space, DWORD minimum, DWORD maximum) {
+            std::vector<DWORD>Locations; bool breakop = false; INT64 mem; if (!space) { mem = 0; }
+            else { mem = minimum; }
+            if (!space) {
+                MEMORY_BASIC_INFORMATION Memory;
+                while (VirtualQueryEx(ProcessHandle, (LPVOID)mem, &mem, sizeof(MEMORY_BASIC_INFORMATION))) {
+                    if (mem.State == MEM_COMMIT) {
+                        std::vector<val> Buffer(Memory.RegionSize);
+                        if (ReadProcessMemory(ProcessHandle, (LPVOID)mem, &Buffer[0], mem.RegionSize, 0)) {
+                            for (size_t i = 0; i < Buffer.size(); ++i) {
+                                for (auto f : Value) {
+                                    if (Buffer[i] == f) {
+                                        Locations.push_back(mem + ((i + 1) * sizeof(val)) - sizeof(val));
+                                    }
+                                }
+                            }
+                        }
+                    } mem += mem.RegionSize;
+                }
+            }
+            else {
+                while (mem < maximum) {
+                    std::vector<val> Buffer(MEMBLOCK);
+                    if (ReadProcessMemory(ProcessHandle, (LPVOID)mem, &Buffer[0], MEMBLOCK, 0)) {
+                        for (size_t i = 0; i < Buffer.size(); ++i) {
+                            if (mem + ((i + 1) * sizeof(val)) - sizeof(val) > maximum) {
+                                breakop = true;
+                                break;
+                            }
+                            for (auto f : Value) {
+                                if (Buffer[i] == f) {
+                                    Locations.push_back(mem + ((i + 1) * sizeof(val)) - sizeof(val));
+                                }
+                            } if (breakop) { break; }
+                        }
+                    } if (breakop) { break; } mem += MEMBLOCK;
+                }
+            }return Locations;
+        }
     }
-  }
-}
-} mem += mem.RegionSize;
-  }
-}
-else {
-while (mem < maximum) {
-std::vector<val> Buffer(MEMBLOCK);
-if (ReadProcessMemory(ProcessHandle, (LPVOID)mem, &Buffer[0], MEMBLOCK, 0)) {
-for (size_t i = 0; i < Buffer.size(); ++i) {
-if (mem + ((i + 1) * sizeof(val)) - sizeof(val) > maximum) {
-breakop = true;
-break;
-  }
-for (auto f : Value) {
-if (Buffer[i] == f) {
-Locations.push_back(mem + ((i + 1) * sizeof(val)) - sizeof(val));
-  }
-} if (breakop) { break; }
-  }
-} if (breakop) { break; } mem += MEMBLOCK;
-  }
-}return Locations;
-    }
-  }
 }
