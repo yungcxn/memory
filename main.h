63363348d467e80306db243a4e5cc8d25c9a80f7
Jakob <45147090+jakob6124@users.noreply.github.com>
2020-05-31 23:44:19 +0200

    Updated
    I'm not a C++ coder, so don't expect this working well.

--- main.h
+++ main.h
@@ -3,53 +3,82 @@
 #include <codecvt>
 #include <random>
 
-#define MEMBLOCK 4096
-#define OPENFLAGS THREAD_QUERY_INFORMATION | PROCESS_VM_OPERATION | PROCESS_VM_READ | PROCESS_VM_WRITE
-#define SNAPFLAGS TH32CS_SNAPMODULE | TH32CS_SNAPMODULE32
-
 namespace virtualmemory
 {
     namespace memoryScanner
     {
-        template <class val>
-        std::vector<DWORD> ScanProcessMemory(HANDLE ProcessHandle, std::vector<val> Value, bool space, DWORD minimum, DWORD maximum) {
-            std::vector<DWORD>Locations; bool breakop = false; INT64 mem; if (!space) { mem = 0; }
-            else { mem = minimum; }
+        std::vector<DWORD> ScanProcessMemory(HANDLE ProcessHandle, int Value, bool space, DWORD minimum, DWORD maximum) 
+        {
+            std::vector<DWORD>Locations; 
+            bool breakop = false; 
+            INT64 mem; 
+            MEMORY_BASIC_INFORMATION Memory;
+            
+            if (!space) 
+            { 
+            mem = 0; 
+            }
+            else 
+            { 
+            mem = minimum; 
+            }
+            
             if (!space) {
-                MEMORY_BASIC_INFORMATION Memory;
                 while (VirtualQueryEx(ProcessHandle, (LPVOID)mem, &mem, sizeof(MEMORY_BASIC_INFORMATION))) {
-                    if (mem.State == MEM_COMMIT) {
+                    if (mem.State == MEM_COMMIT) 
+                    {
                         std::vector<val> Buffer(Memory.RegionSize);
-                        if (ReadProcessMemory(ProcessHandle, (LPVOID)mem, &Buffer[0], mem.RegionSize, 0)) {
-                            for (size_t i = 0; i < Buffer.size(); ++i) {
-                                for (auto f : Value) {
-                                    if (Buffer[i] == f) {
+                        if (ReadProcessMemory(ProcessHandle, (LPVOID)mem, &Buffer[0], mem.RegionSize, 0)) 
+                        {
+                            for (size_t i = 0; i < Buffer.size(); ++i) 
+                            {
+                                for (int f : ) {
+                                    if (Buffer[i] == f) 
+                                    {
                                         Locations.push_back(mem + ((i + 1) * sizeof(val)) - sizeof(val));
                                     }
                                 }
                             }
                         }
-                    } mem += mem.RegionSize;
+                    } 
+                    mem += mem.RegionSize;
                 }
             }
-            else {
-                while (mem < maximum) {
-                    std::vector<val> Buffer(MEMBLOCK);
-                    if (ReadProcessMemory(ProcessHandle, (LPVOID)mem, &Buffer[0], MEMBLOCK, 0)) {
-                        for (size_t i = 0; i < Buffer.size(); ++i) {
-                            if (mem + ((i + 1) * sizeof(val)) - sizeof(val) > maximum) {
+            else 
+            {
+                while (mem < maximum) 
+                {
+                    std::vector<val> Buffer(4096);
+                    if (ReadProcessMemory(ProcessHandle, (LPVOID)mem, &Buffer[0], 4096, 0)) 
+                    {
+                        for (size_t i = 0; i < Buffer.size(); ++i) 
+                        {
+                            if (mem + ((i + 1) * sizeof(val)) - sizeof(val) > maximum) 
+                            {
                                 breakop = true;
                                 break;
                             }
-                            for (auto f : Value) {
-                                if (Buffer[i] == f) {
+                            for (int f : Value) 
+                            {
+                                if (Buffer[i] == f) 
+                                {
                                     Locations.push_back(mem + ((i + 1) * sizeof(val)) - sizeof(val));
                                 }
-                            } if (breakop) { break; }
+                            } 
+                            if (breakop) 
+                            { 
+                                break; 
+                            }
                         }
-                    } if (breakop) { break; } mem += MEMBLOCK;
+                    } 
+                    if (breakop) 
+                    { 
+                        break; 
+                    } 
+                    mem += 4096;
                 }
-            }return Locations;
+            }
+            return Locations;
         }
     }
 }
